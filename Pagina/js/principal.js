f4aa6aa1c75016552fbb03bc4e3cf16d7384500c
Alex Mendoza <aams333@hotmail.com>
2019-01-24 10:02:55 -0500

    Carpetas para la pagina

---
+++ Pagina/js/principal.js
@@ -0,0 +1,17 @@
+/*Variables de las imagenes de los eventos*/
+var imgEvento1 = document.getElementById('imgEvento01');
+var imgEvento2 = document.getElementById('imgEvento02');
+var imgEvento3 = document.getElementById('imgEvento03');
+
+/*Variables de los nombres de los eventos*/
+var nombreEvento1 = "EVENTO ABC";
+document.getElementById('nombreEvento01').innerHTML=nombreEvento1;
+var nombreEvento2 = "EVENTO 2";
+document.getElementById('nombreEvento02').innerHTML=nombreEvento2;
+var nombreEvento3 = "EVENTO 3";
+document.getElementById('nombreEvento03').innerHTML=nombreEvento3;
+
+/*Variable de los salones de los eventos*/
+var salonEvento1 = document.getElementById('salonEvento01');
+var salonEvento2 = document.getElementById('salonEvento02');
+var salonEvento3 = document.getElementById('salonEvento03');
